d6351a517691a8817857233b132bba9a5832f282
ahmaddixoide <ahmadmahmood296@gmail.com>
2022-04-22 00:55:10 +0500

    Timer Added on Splash Screen ( MainActivity.java). after 3 seconds the splash screen will end and will go to new activity named as PatientDoctorSelectionActivity.java

---
+++ app/src/main/java/com/atinity/doct/PatientDoctorSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.atinity.doct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientDoctorSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_doctor_selection);
+    }
+}
