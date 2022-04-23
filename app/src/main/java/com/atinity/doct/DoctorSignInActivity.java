c4b5f92de3ecbc05066be65880efd8f7bff6e3ac
ahmaddixoide <ahmadmahmood296@gmail.com>
2022-04-23 15:06:08 +0500

    added login screen, with UI added new activity to compare dr and patient

---
+++ app/src/main/java/com/atinity/doct/DoctorSignInActivity.java
@@ -0,0 +1,14 @@
+package com.atinity.doct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_sign_in);
+    }
+}
--- app/src/main/java/com/atinity/doct/PatientDoctorSelectionActivity.java
+++ app/src/main/java/com/atinity/doct/PatientDoctorSelectionActivity.java
@@ -2,13 +2,25 @@ package com.atinity.doct;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientDoctorSelectionActivity extends AppCompatActivity {
-
+    private Button selctDoctor,selectPatient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_doctor_selection);
+        selctDoctor=findViewById(R.id.buttonSelectDoctor);
+        selectPatient=findViewById(R.id.buttonSelectPatient);
+
+    }
+
+
+    public void selectDocotor(View view)
+    {
+        startActivity(new Intent(getApplicationContext(),DoctorSignInActivity.class));
     }
 }
