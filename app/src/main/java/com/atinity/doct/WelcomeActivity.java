6ecfa77d62b545635c9e024532621f82691ffaf1
ahmaddixoide <ahmadmahmood296@gmail.com>
2022-04-24 01:41:55 +0500

    Added Sliding Intro Screen which shows only once Corrected Error of shutdown

--- app/src/main/java/com/atinity/doct/WelcomeActivity.java
+++ app/src/main/java/com/atinity/doct/WelcomeActivity.java
@@ -32,8 +32,8 @@
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            requestWindowFeature(Window.FEATURE_NO_TITLE);
-           // getSupportActionBar().hide();
+            //requestWindowFeature(Window.FEATURE_NO_TITLE);
+           //getSupportActionBar().hide();
             // Checking for first time launch - before calling setContentView()
             prefManager = new PrefManager(this);
             if (!prefManager.isFirstTimeLaunch()) {
